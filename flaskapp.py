0dc4bf5fe33faba1f5d6a2cda3b8ec663d40d2b6
swssqewe12 <david@callanan.ie>
2016-03-22 15:44:42 +0000

    Build 34
    - Added /get page to server-side script

--- flaskapp.py
+++ flaskapp.py
@@ -1,6 +1,5 @@
 from flask import Flask
-import jinja2
-import os
+import jinja2, os, random
 
 #import MySQLdb as mysql
 # mysql.connect(host='127.5.239.2',user="adminIChJ87N",passwd="9XHzRbXK1359",db = "python")
@@ -21,6 +20,11 @@ def hello_world():
 	template = JINJA_ENVIRONMENT.get_template('index.html')
 
 	return template.render()
+
+@app.route('/get')
+def sendBackData():
+
+        return 'animal:mammals:dogs:static/dog.png:Dachschund:General household regions' + str(random.randint(1,56))+'_plant:flowers:static/panda.png:Birch Tree:Ireland<br>and other areas!' + str(random.randint(1,4))+random.choice(['','~'])
 
 
 if __name__ == '__main__':
